c795584c69b2ce1d21e358d8b9ef41ed5d655e59
rubenvarela <rubenv6776@gmail.com>
2017-01-24 11:19:11 -0600

    Added and completed albums_migration.sql

---
+++ src/albums_migration.sql
@@ -0,0 +1,28 @@
+-- Create a new file in your called albums_migration.sql.
+-- The first command your albums_migration.sql will need is:
+--  USE codeup_test_db;
+-- Next, use DROP IF EXISTS to (optionally) delete a table called albums.
+-- After dropping in your SQL script, create an albums table with the following columns:
+-- id — auto incrementing unsigned integer primary key
+-- artist — string for storing the recording artist name
+-- name — string for storing a record name
+-- release_date — integer representing year record was released (Note: MySQL also supports a YEAR data type!)
+-- sales — floating point value for number of records sold (in millions)
+-- genre — string for storing the record's genre(s)
+-- Run the script with
+--  mysql -u codeup -p < albums_migration.sql
+-- After running the script, connect to the MySQL client normally.
+-- USE the codeup_test_db and use DESCRIBE and SHOW CREATE to verify that your albums table has been successfully created.
+-- If everything looks good, remember to git push your changes!
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(256) NOT NULL,
+  record_name VARCHAR(256) NOT NULL,
+  release_date DATE NOT NULL,
+  sales int(11) NOT NULL,
+  genre VARCHAR(256),
+  PRIMARY KEY (id)
+)
